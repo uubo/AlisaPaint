f415830160929667cdf97a21c5b63434ae807bda
Vlad Agiyevich <sky-mart@hotmail.com>
2014-05-02 19:48:50 +0400

    Добавил функции поиска юзера, добавления в друзья, получения списка друзей на стороне клиента.

--- AlisaPaint/AlisaPaint/AlisaHttpRequestViewController.m
+++ AlisaPaint/AlisaPaint/AlisaHttpRequestViewController.m
@@ -11,7 +11,9 @@
 @interface AlisaHttpRequestViewController () <NSURLConnectionDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *loginField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UITextField *friendLoginField;
 @property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @property (strong, nonatomic) NSString *currentGoal;
 @end
@@ -20,11 +22,17 @@
 
 NSString *server = @"http://localhost:8080/web_war_exploded/alisa";
 
-NSString *goalParameter = @"goal";
-NSString *registrationGoal = @"reg";
-NSString *authorizationGoal = @"auth";
-NSString *loginParameter = @"login";
-NSString *passwordParameter = @"pass";
+static NSString *goalParameter = @"goal";
+static NSString *registrationGoal = @"reg";
+static NSString *authorizationGoal = @"auth";
+static NSString *findUserGoal = @"find";
+static NSString *addFriendGoal = @"add";
+static NSString *getFriendsGoal = @"getf";
+
+static NSString *loginParameter = @"login";
+static NSString *passwordParameter = @"pass";
+static NSString *userLoginParameter = @"user_login";
+static NSString *friendLoginParameter = @"friend_login";
 
 - (IBAction)registration
 {
@@ -47,6 +55,43 @@ NSString *passwordParameter = @"pass";
                                        goalParameter, self.currentGoal,
                                        loginParameter, self.loginField.text,
                                        passwordParameter, self.passwordField.text]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLConnection *connection = [NSURLConnection connectionWithRequest:request delegate:self];
+    [connection start];
+}
+
+- (IBAction)findUser
+{
+    self.currentGoal = findUserGoal;
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@?%@=%@&%@=%@",
+                                       server,
+                                       goalParameter, self.currentGoal,
+                                       loginParameter, self.loginField.text]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLConnection *connection = [NSURLConnection connectionWithRequest:request delegate:self];
+    [connection start];
+}
+
+- (IBAction)addFriend
+{
+    self.currentGoal = addFriendGoal;
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@?%@=%@&%@=%@&%@=%@",
+                                       server,
+                                       goalParameter, self.currentGoal,
+                                       userLoginParameter, self.loginField.text,
+                                       friendLoginParameter, self.friendLoginField.text]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLConnection *connection = [NSURLConnection connectionWithRequest:request delegate:self];
+    [connection start];
+}
+
+- (IBAction)getFriends
+{
+    self.currentGoal = getFriendsGoal;
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@?%@=%@&%@=%@",
+                                       server,
+                                       goalParameter, self.currentGoal,
+                                       loginParameter, self.loginField.text]];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     NSURLConnection *connection = [NSURLConnection connectionWithRequest:request delegate:self];
     [connection start];
@@ -85,8 +130,59 @@ NSString *passwordParameter = @"pass";
                     self.resultLabel.text = @"Unknown error occured";
                     break;
             }
+        } else if ([self.currentGoal isEqualToString:findUserGoal]) {
+            switch (status) {
+                case 0:
+                    self.resultLabel.text = @"Error occured";
+                    break;
+                case 1:
+                    self.resultLabel.text = @"User has been found";
+                    break;
+                case 2:
+                    self.resultLabel.text = @"User hasn't been found";
+                    break;
+                default:
+                    self.resultLabel.text = @"Unknown error occured";
+                    break;
+            }
+        } else if ([self.currentGoal isEqualToString:addFriendGoal]) {
+            switch (status) {
+                case 0:
+                    self.resultLabel.text = @"Error occured";
+                    break;
+                case 1:
+                    self.resultLabel.text = @"Friend has been added";
+                    break;
+                case 2:
+                    self.resultLabel.text = @"Friend hasn't been added";
+                    break;
+                default:
+                    self.resultLabel.text = @"Unknown error occured";
+                    break;
+            }
+        } else if ([self.currentGoal isEqualToString:getFriendsGoal]) {
+            switch (status) {
+                case 0:
+                    self.resultLabel.text = @"Error occured";
+                    break;
+                case 1:
+                    self.resultLabel.text = @"Friends have been sent";
+                    break;
+                case 2:
+                    self.resultLabel.text = @"Friends haven't been sent";
+                    break;
+                default:
+                    self.resultLabel.text = @"Unknown error occured";
+                    break;
+            }
         }
     }
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    NSString *friendList = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    self.textView.text = friendList;
 }
 
 - (IBAction)hideKeybord:(UITextField *)sender
